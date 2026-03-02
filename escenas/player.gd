06b1339542259bcf4ad6cafdc33cb3893c0bd9de
WIRTZ\lrr <rubenleisfp@gmail.com>
2026-03-02 18:21:05 +0100

    Jugador finalizado

--- escenas/player.gd
+++ escenas/player.gd
@@ -1,4 +1,5 @@
-extends Area2D
+extends Area2D 
+signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -38,3 +39,16 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
