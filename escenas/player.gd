84209508274fbcfd5f437d08e79a8f19920eacb3
WIRTZ\lrr <rubenleisfp@gmail.com>
2026-03-06 19:20:09 +0100

    Escena principal del tutorial

--- escenas/player.gd
+++ escenas/player.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
-	print(velocity.length())
+	#print(velocity.length())
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
